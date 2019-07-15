d0c6f8d23225d937971ebbace90f77d76b5c51fe
jxs <2722961837@qq.com>
2019-07-15 14:24:02 +0800

    updata

--- src/pages/home/questions/testclassify/IndexPage.js
+++ src/pages/home/questions/testclassify/IndexPage.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Modal, Button, Input, Table} from 'antd';
+import { Modal, Button, Input, Table, message,Spin } from 'antd';
 import { connect } from 'dva';
 import './IndexPage.scss'
 const columns = [
     {
         title: '类型ID',
         dataIndex: 'questions_type_id',
-        // render: text => <a>{text}</a>,
     },
     {
         title: '类型名称',
@@ -29,11 +28,25 @@ const rowSelection = {
     }),
 };
 class Adduser extends Component {
-
+    state = { visible: false, value: '' };
     state = { visible: false };
     showModal = () => {
         this.setState({
             visible: true,
+        });
+    };
+
+    handleOk = e => {
+        let { questions } = this.props;
+        if(this.state.value!==''){
+            this.props.sertQuestions({
+            text: this.state.value,
+            sort: JSON.stringify(questions.length + 1)
+        })
+        }
+        message.info('数据插入成功');
+        this.setState({
+            visible: false,
         });
     };
 
@@ -47,7 +60,7 @@ class Adduser extends Component {
         this.props.getData()
     };
     render() {
-        let { questions } = this.props
+        let { questions } = this.props;
         console.log(this.props.questions)
         return (
             <div className="content">
@@ -59,7 +72,7 @@ class Adduser extends Component {
                     <Modal
                         title="创建新类型"
                         visible={this.state.visible}
-                        onOk={this.handleOk}
+                        onOk={this.handleOk} 
                         onCancel={this.handleCancel}>
                         <Input placeholder="请输入类型名称" value={this.state.value} onChange={(e) => {
                             this.setState({
@@ -68,7 +81,6 @@ class Adduser extends Component {
                         }}></Input>
                     </Modal>
                     <Table rowSelection={rowSelection} rowKey='questions_type_id' columns={columns} dataSource={questions} />
-
                 </div>
             </div>
         );
@@ -88,11 +100,19 @@ const mapDisaptchToProps = dispatch => {
             dispatch({
                 type: 'user/getDatas'
             })
+        },
+        sertQuestions(payload) {
+            dispatch({
+                type: 'addtext/sertQuestions',
+                payload
+            })
         }
     }
 }
 
 export default connect(mapStateToProps, mapDisaptchToProps)(Adduser)
+
+
 
 
 
